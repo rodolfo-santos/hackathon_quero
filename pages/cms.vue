36a2c244b6c9e1f1a558f51a77dcc91479b425db
Carlos Oliveira <ceoliveira.dev@gmail.com>
2023-08-19 18:22:41 -0300

    Feat: Update_Functions_Simple

--- pages/cms.vue
+++ pages/cms.vue
@@ -1,48 +1,68 @@
 <script setup>
+import { ref, watch } from 'vue';
 
-import { ref } from 'vue';
+const variable = ref('');
 
+watch(variable, () => {});
+
+const data = computed(() => {
+  return '';
+});
+
+onMounted(() => {});
+
+const links = ['Dashboard', 'Messages', 'Profile', 'Updates'];
 
 const form = ref({
   title: '',
   footerContent: '',
+  author: '',
 });
 
+function clearForm() {
+  form.value.title = '';
+  form.value.footerContent = '';
+  form.value.author = '';
+  console.log('teste');
+}
 
-const submitForm = () => {
-  // Aqui você pode implementar a lógica para enviar os dados do formulário
-  // Por exemplo, enviar os dados para uma API ou salvá-los localmente
-  console.log('Formulário enviado:', form.value);
-};
-
-const links = ['Dashboard', 'Messages', 'Profile', 'Updates'];
+function submitForm() {
+  console.log('Chamada para API');
+}
 </script>
 
 <template>
   <v-app id="inspire">
-
-    <v-app-bar flat class="bg-orange-darken-3">
-
+    <v-app-bar flat class="bg-orange-darken-3 text-white">
       <v-container class="mx-auto d-flex align-center justify-center">
         <v-avatar class="me-4" color="grey-darken-1" size="32"></v-avatar>
 
-        <v-btn v-for="link in links" :key="link" :text="link" variant="text"></v-btn>
+        <v-btn v-for="link in links" :key="link" :text="link" class="font-italic" variant="text"></v-btn>
 
         <v-spacer></v-spacer>
 
         <v-responsive max-width="160">
-          <v-text-field density="compact" flat hide-details label="Search" rounded="lg" single-line variant="solo-filled"></v-text-field>
+          <v-text-field
+            class="font-weight-bold"
+            density="compact"
+            flat
+            hide-details
+            label="Search"
+            rounded="lg"
+            single-line
+            variant="solo-filled"
+            append-inner-icon="mdi-magnify"></v-text-field>
         </v-responsive>
       </v-container>
     </v-app-bar>
 
-    <v-main class="bg-grey-lighten-3">
+    <v-main class="bg-grey-darken-3">
       <v-container>
         <v-row>
           <v-col cols="2">
             <v-sheet rounded="lg">
-              <v-list rounded="lg">
-                <v-list-item v-for="n in 5" :key="n" link :title="`List Item ${n}`"></v-list-item>
+              <v-list rounded="lg" class="bg-grey-darken-2">
+                <v-list-item v-for="n in 5" :key="n" link :title="`List Item ${n}`" class="text-white"></v-list-item>
 
                 <v-divider class="my-2"></v-divider>
 
@@ -52,7 +72,7 @@ const links = ['Dashboard', 'Messages', 'Profile', 'Updates'];
           </v-col>
 
           <v-col>
-            <v-sheet min-height="70vh" rounded="lg" class="pa-10">
+            <v-sheet min-height="70vh" rounded="lg" class="pa-10 bg-grey-darken-2">
               <v-form @submit.prevent="submitForm">
                 <v-row gap="20">
                   <v-col cols="11">
@@ -61,24 +81,29 @@ const links = ['Dashboard', 'Messages', 'Profile', 'Updates'];
                       variant="filled"
                       clear-icon="mdi-close-circle"
                       clearable
-                      label="Título"
+                      label="Título do Blog"
                       type="text"
                       @click:append="sendMessage"
                       @click:clear="clearMessage"></v-text-field>
                   </v-col>
                   <v-col cols="1">
-                    <v-btn icon>
+                    <v-btn icon class="bg-orange-darken-3">
                       <v-icon>mdi-robot-confused-outline</v-icon>
                       <v-tooltip activator="parent" location="start">Gerar título por Inteligência Artificial </v-tooltip>
                     </v-btn>
                   </v-col>
                 </v-row>
 
-
-                <v-textarea v-model="form.footerContent" clear-icon="mdi-close-circle" clearable label="Conteúdo" required auto-grow></v-textarea>
-
+                <v-textarea
+                  v-model="form.footerContent"
+                  class="text-white"
+                  clear-icon="mdi-close-circle"
+                  clearable
+                  label="Conteúdo do Blog"
+                  required
+                  auto-grow></v-textarea>
                 <v-text-field
-                  v-model="form.title"
+                  v-model="form.author"
                   :prepend-icon="icon"
                   variant="filled"
                   clear-icon="mdi-close-circle"
@@ -87,10 +112,8 @@ const links = ['Dashboard', 'Messages', 'Profile', 'Updates'];
                   type="text"></v-text-field>
               </v-form>
               <div class="d-flex justify-end">
-
-                <v-btn type="clear" class="font-weight-bold bg-grey-darken-3 text-orange-darken-3 mr-3">Limpar</v-btn>
-                <v-btn type="submit" class="font-weight-bold text-grey-darken-3 bg-orange-darken-3">Salvar</v-btn>
-
+                <v-btn type="submit" class="font-weight-bold bg-grey-darken-3 text-white mr-3" @click="clearForm">Limpar</v-btn>
+                <v-btn type="submit" class="font-weight-bold text-white bg-orange-darken-3" @click="submitForm">Salvar</v-btn>
               </div>
             </v-sheet>
           </v-col>
