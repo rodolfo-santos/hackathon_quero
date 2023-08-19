a2c080cd5ba844f632b7415cb83cb3aa9f80a876
Rodolfo Santos <rodolfodossantos29@gmail.com>
2023-08-19 17:37:56 -0300

    ✨ create index page to redirect

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app class="px-3" flat dense>
+      <v-avatar color="grey darken-1" class="hidden-md-and-up" size="32"></v-avatar>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <div class="d-flex justify-center my-8">Nada Aqui</div>
+
+        <v-row class="d-flex justify-center">
+          <v-btn to="/cms" class="mr-4" text> CMS </v-btn>
+          <v-btn to="/blog" text> Blog </v-btn>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <v-footer app class="text-center">
+      <!-- Conteúdo do rodapé aqui -->
+    </v-footer>
+  </v-app>
+</template>
