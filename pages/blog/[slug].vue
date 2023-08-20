84e844f7127f8b2c0285ac656be7fde463176cc0
Rodolfo Santos <rodolfodossantos29@gmail.com>
2023-08-20 01:55:07 -0300

    feat: adding parallax

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import Logo from '@/assets/LogoMelhorEscolha.png';
 import { useBlogService } from '@/services/useBlogService';
 
 const links = ['Dashboard', 'Messages', 'Profile', 'Updates'];
@@ -12,21 +13,39 @@ const { data: article } = useAsyncData('article', async () => {
 </script>
 
 <template>
-  <v-app id="inspire">
-    <v-app-bar class="px-3" flat density="compact">
-      <v-avatar color="grey-darken-1" class="hidden-md-and-up" size="32"></v-avatar>
+  <v-app>
+    <v-app-bar flat class="bg-orange-darken-4 text-white">
+      <v-container class="d-flex align-center justify-space-between">
+        <v-avatar class="me-4" size="100">
+          <v-img :src="Logo" alt="Logotipo Melhor Escolha" :width="120"></v-img>
+        </v-avatar>
 
-      <v-spacer></v-spacer>
+        <div class="d-flex justify-center">
+          <v-btn v-for="link in links" :key="link" :text="link" variant="plain"></v-btn>
+        </div>
 
-      <v-tabs centered color="grey-darken-2">
-        <v-tab v-for="link in links" :key="link" :text="link"></v-tab>
-      </v-tabs>
-      <v-spacer></v-spacer>
+        <div>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
 
-      <v-avatar class="hidden-sm-and-down" color="grey-darken-1" size="32"></v-avatar>
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </div>
+      </v-container>
     </v-app-bar>
 
-    <v-main class="bg-grey-lighten-3">
+    <v-main class="bg-gray">
+      <div class="parallax-container">
+        <div class="parallax-image"></div>
+        <div class="overlay"></div>
+        <div class="content">
+          <h1>{{ article.title }}</h1>
+          <p>Aqui irei descrever brevemente do que se trata o artigo</p>
+        </div>
+      </div>
       <v-container>
         <v-row>
           <v-col cols="9">
@@ -34,12 +53,7 @@ const { data: article } = useAsyncData('article', async () => {
               <v-container class="pa-5">
                 <v-row>
                   <v-col cols="12">
-                    <h1 class="text-h3">{{ article.title }}</h1>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="12">
-                    <div v-html="article.content"></div>
+                    <div v-html="article.content" class="article__content"></div>
                     <footer class="mt-4">
                       <p class="text-caption">Com carinho, Equipe Escolar</p>
                     </footer>
@@ -163,10 +177,56 @@ const { data: article } = useAsyncData('article', async () => {
   </v-app>
 </template>
 
-<style scoped>
-h2 {
-  margin-top: 2rem;
+<style>
+.article__content h1,
+.article__content h2,
+.article__content h3,
+.article__content h4 {
+  margin-top: 24px !important;
+  margin-bottom: 8px !important;
 }
+
+.article__content {
+  text-align: justify;
+}
+
+.parallax-container {
+  position: relative;
+  width: 100%;
+  height: 300px;
+  overflow: hidden;
+}
+.parallax-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('https://png.pngtree.com/background/20230517/original/pngtree-classroom-school-room-wallpaper-for-desktop-or-your-smart-phone-by-picture-image_2636726.jpg');
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Ajuste o valor do alpha conforme necessário */
+}
+.content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  text-align: center;
+}
+
+/*-------------------- */
+
 .email {
   margin-top: 0;
 }
@@ -184,15 +244,7 @@ h2 {
 .next {
   margin-left: 220px;
 }
-a:hover {
-  color: orangered;
-  transition: 0.5s;
-}
-.send:hover {
-  background-color: orangered;
-  transition: 0.5s;
-  border: solid 0.5px;
-}
+
 .inputs {
   margin-top: 15px;
 }
@@ -234,11 +286,5 @@ h4 {
 }
 .full-price {
   text-decoration: line-through;
-}
-.card {
-  background-color: orangered;
-}
-.recommended-schools {
-  background-color: orangered;
 }
 </style>
