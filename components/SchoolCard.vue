d9d879cde354ea5f8855b7d2302c4501ea6b3ceb
Rodolfo Santos <rodolfodossantos29@gmail.com>
2023-08-20 02:44:34 -0300

    feat: create school card

---
+++ components/SchoolCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card class="school-card pa-4">
+    <div class="mb-4">
+      <div class="d-flex align-center">
+        <span class="image mr-2"></span>
+        <strong>Ensino Fundamental I - 1ª Série ~ Manhã</strong>
+      </div>
+    </div>
+    <div class="d-flex justify-space-between">
+      <div>R$ 1250</div>
+
+      <v-btn class="bg-orange-darken-4">Ver Bolsa</v-btn>
+    </div>
+  </v-card>
+</template>
+
+<style scoped>
+.school-card {
+  cursor: pointer;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  transition: all 0.2s ease-in-out;
+}
+
+.school-card:hover {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+}
+
+.image {
+  height: 20px;
+  width: 20px;
+  background-image: url('https://yt3.googleusercontent.com/ytc/AOPolaRoCGlNij_VyMdkm2z9SC4AmqnfBD0kpCoTPV-g=s900-c-k-c0x00ffffff-no-rj');
+  background-size: cover;
+  background-position: center;
+}
+</style>
--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import Logo from '@/assets/LogoMelhorEscolha.png';
+import SchoolCard from '@/components/SchoolCard.vue';
 import { useBlogService } from '@/services/useBlogService';
 
 const links = ['Dashboard', 'Messages', 'Profile', 'Updates'];
@@ -37,7 +38,7 @@ const { data: article } = useAsyncData('article', async () => {
       </v-container>
     </v-app-bar>
 
-    <v-main class="bg-gray">
+    <v-main>
       <div class="parallax-container">
         <div class="parallax-image"></div>
         <div class="overlay"></div>
@@ -64,106 +65,19 @@ const { data: article } = useAsyncData('article', async () => {
           </v-col>
 
           <v-col cols="3">
-            <v-sheet rounded="lg" min-height="268">
-              <p class="card">Escolas com bolsas disponíveis para seu filho. Venha conferir!</p>
-              <div class="recommended-schools">
-                <div class="recommended-schools-list">
-                  <div class="Colegio">
-                    <a class="schools" href="/">
-                      <img class="logo" src="https://moppe.com.br/wp-content/uploads/2021/09/cropped-cropped-LogoM512-2.png" />
-                      <h4>Moppe</h4>
-                      <div class="prices">
-                        <span class="Ev">A partir de</span>
-                        <span class="full-price"> 1658,25</span>
-                        <span class="offered-price">825,50</span>
-                      </div>
-                    </a>
-                  </div>
-
-                  <div class="Colegio">
-                    <a class="schools" href="/">
-                      <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Melhor_Escolha_logo.png" />
-                      <h4>Melhor escolha</h4>
-                      <div class="prices">
-                        <span class="Ev">A partir de</span>
-                        <span class="full-price"> 1430,20</span>
-                        <span class="offered-price">754,80</span>
-                      </div>
-                    </a>
-                  </div>
-                  <div class="Colegio">
-                    <a class="schools" href="/">
-                      <img class="logo" src="https://colegiobelavistasp.com.br/wp-content/uploads/2019/01/Logo-Objetivo-PDF.png" />
-                      <h4>Objetivo</h4>
-                      <div class="prices">
-                        <span class="Ev">A partir de</span>
-                        <span class="full-price"> 1587,50</span>
-                        <span class="offered-price">690,90</span>
-                      </div>
-                    </a>
-                  </div>
-                  <div class="Colegio">
-                    <a class="schools" href="/">
-                      <img
-                        class="logo"
-                        src="https://static.wixstatic.com/media/d36955_ba84321a9a564771bc490dd5dedf3f83~mv2.png/v1/fill/w_600,h_180,al_c,q_85,usm_0.33_1.00_0.00,enc_auto/Logo_Poliedro_Horiz_SIST_ENSINO.png" />
-                      <h4>Poliedro</h4>
-                      <div class="prices">
-                        <span class="Ev">A partir de</span>
-                        <span class="full-price"> 1330,40</span>
-                        <span class="offered-price">830,15</span>
-                      </div>
-                    </a>
-                  </div>
-                </div>
+            <div class="bg-grey-lighten-3 py-8 px-4">
+              <p class="mb-4">Estude na escola <strong>Escola de Teste</strong> com mensalidades que cabem no seu bolso</p>
+              <div style="display: flex; gap: 12px; flex-direction: column">
+                <SchoolCard />
+                <SchoolCard />
+                <SchoolCard />
+                <SchoolCard />
               </div>
-            </v-sheet>
+            </div>
           </v-col>
         </v-row>
       </v-container>
     </v-main>
-
-    <v-row class="inputs">
-      <v-col cols="4"></v-col>
-      <v-col cols="4">
-        <v-form>
-          <v-row>
-            <v-col cols="4">
-              <v-text-field :counter="15" label="First name" required></v-text-field>
-            </v-col>
-
-            <v-col cols="4">
-              <v-text-field :counter="15" label="Last name" required></v-text-field>
-            </v-col>
-            <v-col class="email" cols="4">
-              <v-text-field label="E-mail" required></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row class="coment">
-            <v-col cols="12" md="12">
-              <v-textarea label="Comentário"></v-textarea>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" md="8">
-              <v-btn class="send" type="submit" blockclass="mt-2">Submit</v-btn>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="4">
-              <p>Anterior</p>
-              <a class="link" href="/">A escola foi decorada toda com coelhos na páscoa</a>
-            </v-col>
-            <v-col cols="4"> </v-col>
-            <v-col cols="4">
-              <p class="next">Próxima</p>
-              <a class="link" href="/">A escola fez um evento de games para os alunos</a>
-            </v-col>
-          </v-row>
-        </v-form>
-      </v-col>
-      <v-col cols="4"></v-col>
-    </v-row>
 
     <v-footer class="text-center d-flex flex-column">
       <div>
@@ -223,68 +137,5 @@ const { data: article } = useAsyncData('article', async () => {
   transform: translate(-50%, -50%);
   color: white;
   text-align: center;
-}
-
-/*-------------------- */
-
-.email {
-  margin-top: 0;
-}
-.coment {
-  margin-bottom: 3px;
-}
-.send {
-  width: 200px;
-  align-items: center;
-}
-.link {
-  text-decoration: none;
-  color: white;
-}
-.next {
-  margin-left: 220px;
-}
-
-.inputs {
-  margin-top: 15px;
-}
-.logo {
-  max-width: 50px;
-  max-height: 50px;
-}
-.recommended-schools-list {
-  padding: 15px;
-}
-.prices {
-  justify-content: space-between;
-  padding: 15px;
-}
-.full-price {
-  margin-left: 140px;
-}
-.offered-price {
-  margin-left: 30px;
-}
-.schools {
-  text-decoration: none;
-  color: grey;
-  padding: 15px;
-}
-.Colegio {
-  margin-bottom: 20px;
-  background-color: aliceblue;
-  border-radius: 15px;
-}
-.Colegio:hover {
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-}
-h4 {
-  padding: 15px;
-}
-.logo {
-  margin-top: 10px;
-}
-.full-price {
-  text-decoration: line-through;
 }
 </style>
